5967f88f047659c25400b0acaf323e06efb6d506
OlachiEfika <38718817+OlachiEfika@users.noreply.github.com>
2022-09-06 20:09:27 -0400

    Update Update.py

--- Update.py
+++ Update.py
@@ -1,1 +1,2 @@
 # Today I am learning about Github workflows and branches
+print(" Today I learned how to create a branch, edit and commit changes in that branch, open a pull request, and merge the pull request into the main project")
