03e1f9babff892b71d8e84e4281447401cb1575d
OlachiEfika <38718817+OlachiEfika@users.noreply.github.com>
2022-09-08 13:23:21 -0400

    Update Update.py

--- Update.py
+++ Update.py
@@ -1,2 +1,6 @@
 # Today I am learning about Github workflows and branches
-print(" Today I learned how to create a branch, edit and commit changes in that branch, open a pull request, and merge the pull request into the main project")
+#Today I learned how to create a branch, edit and commit changes in that branch, open a pull request, and merge the pull request into the main project
+# Today I completed both parts of the final project on Getting Started with Git and Github provided by IBM. 
+# As part of the project, I created an open source project in Git, made changes to that project and made it available to the community. 
+# I forked a GitHub repository, cloned that repository to my local system, made changes to the local repository and committed the changes locally
+# Finally I pushed the commits it back to my GitHub fork, and create a pull request to add your update to the original repository.
